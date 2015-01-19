e1165aa3cd0e222fb66b4c28375e90688fe1eb6b
Hamed Asemi <hamed.asemi@silobreaker.com>
2015-01-20 00:30:17 +0100

    Bug fixed on seperator line next line

--- tasks/support.js
+++ tasks/support.js
@@ -5,15 +5,15 @@ gulp.task('support:tasks', function() {
     var colors = require('chalk');
     console.log(
         colors.yellow('\nAvailable tasks and sub tasks') +
-        '-----------------------------\n'
+        '\n-----------------------------\n'
     );
 
     for (var task in gulp.tasks) {
         console.log('   ' + colors.bold.blue(gulp.tasks[task].name));
     }
     console.log(
-        colors.yellow('\n As an example you can run\n') +
-        '--------------------------\n' +
+        colors.yellow('\n As an example you can run') +
+        '\n--------------------------\n' +
         colors.bold.black('   ' + '$ iio ') +
         colors.gray.bold('js less:watch\n')
     );
