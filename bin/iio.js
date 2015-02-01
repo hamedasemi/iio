1003a1f4130c9dddb0a142077f7edc145a9329d0
Hamed Asemi <hamed.asemi@silobreaker.com>
2015-02-01 22:32:33 +0100

    Merging user and default config file fixed with "Deep Merging"

--- bin/iio.js
+++ bin/iio.js
@@ -1,26 +1,29 @@
 #!/usr/bin/env node
 
+var _ = require("underscore");
 var cwd = process.cwd();
-
 var colors = require('chalk');
+
 console.log(colors.bold.cyan.underline('\nYap! iio is fast\n'));
 
-var config = {};
+try {
+    var userConfig = require(cwd + '/iio.config');
+} catch (ex) {
+    var userConfig = {};
+}
 
 var defaultConfig = require(cwd + '/defaults/iio.config');
-var userConfig = require(cwd + '/iio.config');
-
-for (var attrname in defaultConfig) {
-    config[attrname] = defaultConfig[attrname];
-}
-for (var attrname in userConfig) {
-    config[attrname] = userConfig[attrname];
-}
 
 var tasks = [];
 for (var task in userConfig) {
     tasks.push(task);
 }
+
+// Override basic _.defaults
+_.defaults = require('merge-defaults');
+
+var config = _.defaults(userConfig, defaultConfig);
+
 config.tasks = tasks;
 
 var iio = require('./../index.js')(config);
