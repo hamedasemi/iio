1b06bb077e06e14519249581e1784d600f6bd452
Hamed Asemi <hamed.asemi@silobreaker.com>
2015-02-05 23:48:35 +0100

    BrowserSync easier task command

--- tasks/browsersync.js
+++ tasks/browsersync.js
@@ -2,6 +2,43 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync'),
     conf = gulp.config.browsersync;
 
+var _ = require("underscore");
+
+// Override basic _.defaults
+_.defaults = require('merge-defaults');
+
+gulp.task('browser', function() {
+    browserSync(_.defaults({
+            open: true,
+            codeSync: false
+        },
+        conf.options.options
+    ));
+});
+
+gulp.task('browser:ext', function() {
+    browserSync(_.defaults({
+            open: "external",
+            codeSync: false
+        },
+        conf.options.options
+    ));
+});
+
 gulp.task('browser:sync', function() {
-    browserSync(conf.options.options);
+    browserSync(_.defaults({
+            open: true,
+            codeSync: true
+        },
+        conf.options.options
+    ));
+});
+
+gulp.task('browser:ext:sync', function() {
+    browserSync(_.defaults({
+            open: "external",
+            codeSync: true
+        },
+        conf.options.options
+    ));
 });
