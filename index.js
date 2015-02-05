2beda8349531c2602e7f72503b40bb4d96c4b705
Hamed Asemi <hamed.asemi@silobreaker.com>
2015-02-05 23:30:32 +0100

    Listing tasks for user

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 module.exports = function(config) {
 
-		var colors = require('colors');
+    var colors = require('colors');
     var gulp = require('gulp');
 
     gulp.config = config;
     config.tasks.push('support');
 
-    for (var task in config.tasks){
-    	eval(config.tasks[task] + ' = require("./tasks/' + config.tasks[task] + '")');
+    console.log(colors.white("  Loading tasks:"));
+    for (var task in config.tasks) {
+        eval(config.tasks[task] + ' = require("./tasks/' + config.tasks[task] + '")');
+        console.log(colors.blue("     " + colors.bold.blue.underline(config.tasks[task]) + ""));
     }
+    console.log(colors.white("  Done! \n"));
     return gulp;
 };
