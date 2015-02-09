d654e3a6f1bd8dae8816a6a653501335ae3a17d4
Hamed Asemi <hamed.asemi@silobreaker.com>
2015-02-09 22:58:59 +0100

    Font face generator added

--- tasks/fontface.js
+++ tasks/fontface.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     rename = require('gulp-rename'),
-    gulpfontgen = require("gulp-fontgen"),
+    gulpfontgen = require("./../plugins/iio-fontface"),
     browserSync = require('browser-sync'),
     reload = browserSync.reload,
     conf = gulp.config.fontface;
@@ -11,8 +11,9 @@ var _ = require("underscore");
 gulp.task('fontface', conf.dependencies, function() {
     gulp.src(conf.src + '/' + conf.type)
         .pipe(gulpfontgen(_.extend({
-            dest: conf.dest.files,
-            css: conf.dest.fontface + '/' + conf.output,
-            css_fontpath: conf.dest.relative
+            ext: conf.output.ext,
+            fontface: conf.dest.fontface,
+            relative: conf.dest.relative,
+            dest: conf.dest.files
         }, conf.options.options)));
 });
